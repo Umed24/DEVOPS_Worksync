fd0c94e1c4e7d69c1c7027a9f2d1a1b2b4ccf88f
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-10-16 20:56:46 +0530

    creating the instance config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,9 @@
-
-# Create a new security group with a unique name
-resource "aws_security_group" "new_allow_http_ssh" {
-  name        = "new_allow_http_ssh"  # Change the name to something unique
-  description = "Allow inbound HTTP, SSH, and application traffic"
-  vpc_id      = "vpc-067575f1870e8da0d"  # Ensure this VPC ID is correct
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 1000
-    to_port     = 1000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Launch the EC2 instance using the newly created security group
+# Launch the EC2 instance using the existing security group
 resource "aws_instance" "worksync_app" {
   ami             = "ami-00f251754ac5da7f0"  # Amazon Linux 2 AMI
   instance_type   = var.instance_type       # Ensure this variable is defined
   key_name        = var.key_name            # Ensure this variable is defined
-  security_groups = [aws_security_group.new_allow_http_ssh.name]  # Use the new security group
+  security_groups = ["sg-0419d5d4c4aa6e799"]  # Use the existing security group ID
 
   user_data = file("userdata.sh")  
   tags = {
