df049ad1c1dcce164406c96bb9fa6a87ba8f29c0
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-10-16 21:03:55 +0530

    creating the instance config

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "worksync_app" {
   ami             = "ami-00f251754ac5da7f0"  # Amazon Linux 2 AMI
   instance_type   = var.instance_type       # Ensure this variable is defined
   key_name        = var.key_name            # Ensure this variable is defined
-  security_groups = ["sg-0419d5d4c4aa6e799"]  # Use the existing security group ID
 
   user_data = file("userdata.sh")  
   tags = {
