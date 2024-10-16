85fb119b752b1056cdfc083b7d44c86ac4481bbe
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-10-16 20:19:17 +0530

    creating the instance config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,20 @@
+provider "aws" {
+  region = "us-east-1"  # Northern Virginia
+}
+
+data "aws_security_group" "allow_http_ssh" {
+  # Replace with your actual VPC ID
+  vpc_id = "vpc-067575f1870e8da0d"
+
+  filter {
+    name   = "group-name"
+    values = ["allow_http_ssh"]
+  }
+}
+
 resource "aws_security_group" "allow_http_ssh" {
+  count = length(data.aws_security_group.allow_http_ssh.ids) == 0 ? 1 : 0
+
   name        = "allow_http_ssh"
   description = "Allow inbound HTTP, SSH, and application traffic"
 
@@ -35,7 +51,7 @@ resource "aws_instance" "worksync_app" {
   ami             = "ami-00f251754c5da7f0"  # Amazon Linux 2 AMI
   instance_type   = var.instance_type
   key_name        = var.key_name
-  security_groups = [aws_security_group.allow_http_ssh.name]
+  security_groups = [aws_security_group.allow_http_ssh.*.name]
 
   user_data = file("userdata.sh")  
   tags = {
