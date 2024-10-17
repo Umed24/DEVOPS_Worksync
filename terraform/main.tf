21e61fc95ef88706fd3cb89e8574756d8fe211f5
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-10-17 20:49:33 +0530

    updated ELK

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 # Launch the EC2 instance using the existing security group
 resource "aws_instance" "worksync_app" {
   ami             = "ami-00f251754ac5da7f0"  # Amazon Linux 2 AMI
-  instance_type   = var.instance_type       # Ensure this variable is defined
-  key_name        = var.key_name            # Ensure this variable is defined
+  instance_type   = var.instance_type       
+  key_name        = var.key_name            
 
   user_data = file("userdata.sh")  
   tags = {
